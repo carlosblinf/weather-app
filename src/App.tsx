641fd19f1543c00c5f80f8e4427051de0c112614
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2024-07-01 07:58:11 -0400

    fix: drop space arround

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen bg-bgLightGrey">
       <Header />
-      <div className="flex-1 flex flex-col gap-12 justify-around items-center mt-10 p-12">
+      <div className="flex-1 flex flex-col gap-12 items-center mt-10 p-12">
         <Search />
         <WrapperCardList />
       </div>
